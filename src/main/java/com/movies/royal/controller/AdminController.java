0353d753af4ac07dc6d00459b23033d71444befe
Rustam-Asdanov <Rustam-Asdanov@users.noreply.github.com>
2022-01-31 18:37:14 +0400

    admin controller is updated

--- src/main/java/com/movies/royal/controller/AdminController.java
+++ src/main/java/com/movies/royal/controller/AdminController.java
@@ -1,9 +1,13 @@
 package com.movies.royal.controller;
 
+import com.movies.royal.model.Movie;
 import com.movies.royal.service.MovieService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -16,5 +20,29 @@ public class AdminController {
     @GetMapping
     public String getPage(){
         return "page---admin";
+    }
+
+    @GetMapping("/addMovie")
+    public String getAddMoviePage(Model model){
+        model.addAttribute("movie",new Movie());
+        return "page---movie---add";
+    }
+
+    @GetMapping("/getMovieBase")
+    public String getMovieBase(Model model){
+        model.addAttribute("movieList",null);
+        return "page---movie---base";
+    }
+
+    @PostMapping("/addMovie")
+    public String addMovie(
+            @ModelAttribute("movie") Movie movie
+    ){
+        return "redirect:/admin_page/getMovieBase";
+    }
+
+    @GetMapping("/updateMovie/{id}")
+    public String updateMovie(){
+        return "";
     }
 }
--- src/main/java/com/movies/royal/controller/MoviesController.java
+++ src/main/java/com/movies/royal/controller/MoviesController.java
@@ -14,4 +14,5 @@ public class MoviesController {
         model.addAttribute("movieList",null);
         return "page---movies";
     }
+
 }
