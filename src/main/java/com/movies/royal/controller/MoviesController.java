dc2a8742a78f259abc6e28bd5f74f9634ac54693
Rustam-Asdanov <Rustam-Asdanov@users.noreply.github.com>
2022-02-01 10:48:46 +0400

    project is ready

--- src/main/java/com/movies/royal/controller/MoviesController.java
+++ src/main/java/com/movies/royal/controller/MoviesController.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -18,6 +19,15 @@ public class MoviesController {
     public String getPage(Model model){
         model.addAttribute("movieList",movieService.getMovieList());
         return "page---movies";
+    }
+
+    @GetMapping("/movieInfo/{id}")
+    public String getMovieInfo(
+            @PathVariable("id") long id,
+            Model model
+    ){
+        model.addAttribute("movie",movieService.getMovieById(id));
+        return "page---movie---info";
     }
 
 }
